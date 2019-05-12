b9f97ff17ebfa610d4c69a48395d426d9b895eb5
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-05-12 09:19:46 +0300

    + DoorTrigger

---
+++ SpaceShooter/Assets/Scripts/AIEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEnemy : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float damage = 1.0f;
+    public float maxDistance = 10.0f;
+    public float minDistance = 1.0f;
+
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
