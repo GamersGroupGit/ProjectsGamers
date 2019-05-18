b18cc792462329bb6248222198f60d6cb2b19000
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-05-18 22:27:17 +0300

    + Scene Controller

--- SpaceShooter/Assets/Scripts/AIEnemy.cs
+++ SpaceShooter/Assets/Scripts/AIEnemy.cs
@@ -12,28 +12,40 @@ public class AIEnemy : MonoBehaviour
     public Transform target;
 
     public PlayerCharacter player;
-    // Start is called before the first frame update
+
+    private bool _alive;//логическая переменная для слежения за жизнью
+
     void Start()
     {
-        
+        _alive = true; //делаем ее в начале истинной
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(transform.position, target.position);
-        if (distance > maxDistance)
+        if (_alive)
         {
-            transform.Translate(0, 0, speed * Time.deltaTime);//непрерывно движемся вперед в каждом кадре
+
+            distance = Vector3.Distance(transform.position, target.position);
+            if (distance > maxDistance)
+            {
+                transform.Translate(0, 0, speed * Time.deltaTime);//непрерывно движемся вперед в каждом кадре
+            }
+            if (distance < maxDistance && distance > minDistance)
+            {
+                transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(target.position - transform.position), speed * Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+            }
+            if (distance < minDistance)
+            {
+                player.Hurt(damage);
+            }
         }
-        if (distance < maxDistance && distance > minDistance)
-        {
-            transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(target.position - transform.position), speed * Time.deltaTime);
-            transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
-        }
-        if (distance < minDistance)
-        {
-            player.Hurt(damage);
-        }
+    }
+
+    public void SetAlive(bool alive)
+    {//открывает метод,который из вне позволяет воздействовать на переменную
+        _alive = alive;
     }
 }
---
+++ SpaceShooter/Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+public GameObject enemyPrefab;//Открытая переменная для связи с префабом
+    private GameObject _enemy;//закрытая переменная для слежения за прифабом врага на сцене
+    private GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    { //порождаем нового врага, если на сцене отсутсвует
+        if (_enemy == null)
+        {
+            _enemy = Instantiate(enemyPrefab) as GameObject; //метод копирующий объект-шаблон
+            _enemy.transform.position = gameObject.transform.position;
+            player = GameObject.FindWithTag("Player");//находим объект с тегом “Player”
+
+        }
+    }
+}
