a0a1b3ec2ddfd0c77854a1b5eb8f7ae461fab2ea
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-05-25 16:20:52 +0300

    + health

--- SpaceShooter/Assets/Scripts/Item.cs
+++ SpaceShooter/Assets/Scripts/Item.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
     public string itemName; //Вводим имена в во вкладку Inspector
     private int _score; // счетчик очков
+
+    public Text scoreLabel;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,7 +17,13 @@ public class Item : MonoBehaviour
         if (itemName == "cube")
         {//проверяем, является ли объект здоровьем
             _score++; //увеличиваем счетчик очков
+            scoreLabel.text = _score.ToString();
             Debug.Log("Score: " + _score);
         }
+    }
+
+    private void Start()
+    {
+        scoreLabel.text = "0";
     }
 }
